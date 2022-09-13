566dca9da6a0ef2799b785c9de14175f3ef304b9
Joel Junström <joel.junstrom@gmail.com>
2022-09-13 15:07:11 +0200

    Initial commit

---
+++ spec/song_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "song"
+
+RSpec.describe Song do
+  describe ".lyrics" do
+    # Find the full lyrics in spec/fixtures/song.txt
+    it "returns the full lyrics of the 99-bottles of beer song" 
+  end
+end
